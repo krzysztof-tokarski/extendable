ca16b750831a15e171d5e6b379bfaab18eb18695
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2022-11-01 10:06:35 +0100

    build: dev-ready material-setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Extendable
+
+1. To install dependencies run **npm ci**.
+2. To launch the devsever run **npm run devstart**.
--- apps/extendable/src/app/app.module.ts
+++ apps/extendable/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, MatCardModule, MatButtonModule, BrowserAnimationsModule],
+  exports: [MatCardModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
